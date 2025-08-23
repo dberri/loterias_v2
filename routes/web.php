329de443c1b128e930a9a6d16b12e0dfafe4589a
David Berri <dwbwill@gmail.com>
2025-08-23 16:50:38 -0300

    Removes pillar pages and keeps regular pages

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,15 @@
 <?php
 
-use App\Models\PillarPage;
 use Illuminate\Support\Facades\Route;
+use Z3d0X\FilamentFabricator\Models\Page;
 
 Route::get('/', function () {
     return view('home');
 });
 
-// Route for pillar pages by game enum value
-Route::get('/{game}', function (string $game) {
-    $page = PillarPage::where('game', $game)
-        ->where('is_published', true)
-        ->firstOrFail();
-    
-    return view("components.filament-fabricator.layouts.{$page->layout}", [
-        'page' => $page
-    ]);
-})->where('game', 'megasena|lotofacil|quina');
-
 // Route for custom pillar pages by slug
 Route::get('/{slug}', function (string $slug) {
-    $page = PillarPage::where('slug', $slug)
-        ->where('is_published', true)
-        ->firstOrFail();
+    $page = Page::where('slug', $slug)->firstOrFail();
     
     return view("components.filament-fabricator.layouts.{$page->layout}", [
         'page' => $page
